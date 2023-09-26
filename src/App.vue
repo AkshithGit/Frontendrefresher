6afd308860ccb7e5be86c26e72c4f0ef394defea
Sudharshanam <sudharshanam@csu.fullerton.edu>
2023-09-26 14:51:15 -0700

    First commit- created a basic structure

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1>
+            My Cool App
+        </h1>
+        <button>
+            Add Task
+        </button>
+    </div>
+
+</template>
+
+<script>
+export default {
+    name: 'App'
+}
+</script>
+
+<style>
+
+</style>
